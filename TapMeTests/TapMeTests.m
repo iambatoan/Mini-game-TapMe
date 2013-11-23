af33be8818d5a9ce4f9b1b3884411c8a04135131
batoan1221 <batoan1221@gmail.com>
2013-11-23 20:36:28 +0700

    Initial Commit

---
+++ TapMeTests/TapMeTests.m
@@ -0,0 +1,34 @@
+//
+//  TapMeTests.m
+//  TapMeTests
+//
+//  Created by admin on 11/23/13.
+//  Copyright (c) 2013 admin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TapMeTests : XCTestCase
+
+@end
+
+@implementation TapMeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
